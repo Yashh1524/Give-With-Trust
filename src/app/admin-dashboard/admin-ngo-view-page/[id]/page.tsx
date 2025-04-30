6ed209a1d03c919a15516905c8f09b65ff874176
YashBhut1524 <yashxerox15@gmail.com>
2025-04-30 22:25:55 +0530

    fix monthlyWorkProof Type

--- src/app/admin-dashboard/admin-ngo-view-page/[id]/page.tsx
+++ src/app/admin-dashboard/admin-ngo-view-page/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { getDonationByNgoId } from '@/actions/donation.action'
 import { getNgoByNgoId } from '@/actions/ngo.action'
 import { getPayoutsByNgoId } from '@/actions/payout.action'
+import { getMonthlyWorkProofsByNgoId } from '@/actions/proofs.action'
 import { getCurrentUserRole } from '@/actions/user.action'
 import AdminNgoViewPageClient from '@/components/AdminNgoViewPageClient'
 import UnauthorizedAccess from '@/components/UnauthorizedAccess'
@@ -24,7 +25,7 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
     const donations = await getDonationByNgoId(ngoId)
     const userRole = await getCurrentUserRole()
     const payouts = await getPayoutsByNgoId(ngoId)
-
+    const monthlyWorkProofs = await getMonthlyWorkProofsByNgoId(ngoId)
     
     return (
         <>
@@ -33,6 +34,7 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
                     ngo={ngo}
                     donations={donations as ExtendedDonation[]}
                     payouts={payouts}
+                    monthlyWorkProofs={monthlyWorkProofs}
                 />
             ) : (
                 <UnauthorizedAccess />
